f421a5b624228b116a5fb8cbb70be46373f34b17
Gurban Gurbanov <qurban.qurbanov93@gmail.com>
2018-04-30 22:14:09 +0400

    Added header as a static prop

--- screens/DetailScreen.js
+++ screens/DetailScreen.js
@@ -2,6 +2,10 @@ import React, {Component} from 'react';
 import {View, Text, Button} from 'react-native';
 
 export default class DetailScreen extends Component {
+    static navigationOptions = {
+        title: 'Detail'
+    };
+
     render() {
         return (
             <View style={{
@@ -10,15 +14,6 @@ export default class DetailScreen extends Component {
                 alignItems: 'center'
             }}>
                 <Text>Detail screen received param: {this.props.navigation.state.params.foo}</Text>
-                <Button
-                    title="Go to detail screen again"
-                    onPress={() => this.props.navigation.navigate('Detail')}
-                />
-
-                <Button
-                    title="Go back"
-                    onPress={() => this.props.navigation.goBack()}
-                />
             </View>
         );
     }
