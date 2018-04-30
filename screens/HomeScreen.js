ee49db98684a277a53e98615b7985f858e80147b
Gurban Gurbanov <qurban.qurbanov93@gmail.com>
2018-05-01 00:08:35 +0400

    Added state manipulation from the header

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,8 +2,49 @@ import React, {Component} from 'react';
 import {View, Text, Button} from 'react-native';
 
 export default class HomeScreen extends Component {
-    static navigationOptions = {
-        title: 'Home'
+    // static navigationOptions = {
+    //     title: 'Home',
+    //     headerRight: (
+    //         <Button
+    //             onPress={() => {
+    //                 alert('Pressed button on the header');
+    //             }}
+    //             title="+1"
+    //             color="white"
+    //         />
+    //     )
+    // };
+
+    static navigationOptions = ({navigation}) => {
+        const params = navigation.state.params || {};
+
+        return {
+            title: 'Home',
+            headerRight: (
+                <Button
+                    title="+1"
+                    color="white"
+                    onPress={params.increaseCount}
+                />
+            )
+        }
+
+    };
+
+    state = {
+        count: 0
+    };
+
+    componentWillMount = () => {
+        this.props.navigation.setParams({
+            increaseCount: this._increaseCount
+        });
+    };
+
+    _increaseCount = () => {
+        this.setState({
+            count: this.state.count + 1
+        });
     };
 
     render() {
@@ -14,6 +55,7 @@ export default class HomeScreen extends Component {
                 alignItems: 'center'
             }}>
                 <Text>Home screen</Text>
+                <Text>State.count: {this.state.count}</Text>
                 <Button
                     title="Go to detail screen"
                     onPress={() => this.props.navigation.navigate('Detail', {foo: 'bar', title: 'Cutsom Title'})}
